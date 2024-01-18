047be022ff21f4631895baa711a45dbc2c1d8939
Masoom-Wahid <masoom3892@gmail.com>
2024-01-18 13:01:58 +0430

    First Integration of /editor with API

--- src/components/ChallengeSolve/Organisms/DescriptionBox.jsx
+++ src/components/ChallengeSolve/Organisms/DescriptionBox.jsx
@@ -1,24 +1,50 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import styles from "./DescriptionBox.module.css";
+import {useNavigate} from 'react-router-dom';
+
+
+const BASE_URL = process.env.REACT_APP_API_URL;
+const QUESTION_ID = 21
+
 
 const DescriptionBox = () => {
   const [tabValue, setTabValue] = useState(0);
+  const [challenge,setChallenge] = useState([]);
 
-  const problemDetails = {
-    title: "Two Sum",
-    difficulty: "Easy",
-    description:
-      "Given an array of integers 'nums' and an integer 'target', return indices of the two numbers such that they add up to 'target'.",
-    examples: [
-      {
-        id: 1,
-        input: "nums = [2, 7, 11, 15], target = 9",
-        output: "[0,1]",
-        explanation: "Because nums[0] + nums[1] == 9, we return [0, 1].",
-      },
-      // ... other examples if any
-    ],
-  };
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}competition/${QUESTION_ID}`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+
+
+      const data = await response.json();
+        if (!response.ok) {
+          console.log(response.status)
+          // 401 means unauthorized , 403 means unauthorized, so the user is either using an old token or is 
+          // either bypassing 
+          if (response.status === 401 || response.status === 403){
+            localStorage.removeItem("accessToken")
+            navigate("/")
+          }
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        setChallenge(data)
+        // Process the data
+      } catch (error) {
+        // Handle errors
+        console.error(error);
+      }
+}
+    fetchData()
+},[]);
+
 
   return (
     <div className={styles.descriptionBox}>
@@ -40,20 +66,23 @@ const DescriptionBox = () => {
           Submissions
         </div>
       </div>
-      {tabValue === 0 && (
+      {/* i tried other ways but for some reason the useEffect would not load quickly , so i had to do it this way
+        see if u can do it better , i am sure there is a better way
+      */}
+      {tabValue === 0 && challenge.question != undefined && (
         <div className={styles.contentBox}>
-          <h5 className={styles.title}>{problemDetails.title}</h5>
+          <h5 className={styles.title}>{challenge.question.title}</h5>
           <div className={styles.difficulty}>
-            Difficulty: {problemDetails.difficulty}
+            Difficulty: {challenge.question.lvl}
           </div>
-          <p className={styles.description}>{problemDetails.description}</p>
-          {problemDetails.examples.map((example) => (
+          <p className={styles.description}>{challenge.question.description}</p>
+          {challenge.test_cases.map((example) => (
             <div key={example.id} className={styles.exampleBox}>
               <p className={styles.exampleInput}>
-                <b>Input:</b> {example.input}
+                <b>Input:</b> {example.sample}
               </p>
               <p className={styles.exampleOutput}>
-                <b>Output:</b> {example.output}
+                <b>Output:</b> {example.answer}
               </p>
               <p className={styles.exampleExplanation}>
                 <b>Explanation:</b> {example.explanation}
