02797645e32ba9c97cbf68c1c370680b27d8bbf4
Masoom-Wahid <masoom3892@gmail.com>
2024-01-19 12:58:34 +0430

    Integration of /challengesdetails and the testcases of it

--- src/components/Dashboard/ChallengeDetails.jsx
+++ src/components/Dashboard/ChallengeDetails.jsx
@@ -1,27 +1,60 @@
-import React from "react";
+import React,{useState} from "react";
 import styles from "./ChallengeDetails.module.css"; // Import the CSS module
+import {useNavigate} from 'react-router-dom';
 
-const ChallengeDetails = () => {
-  // Updated mock data for the challenge details
-  const challengeDetails = {
-    Name: "Sample Challenge",
-    Level: "Easy",
-    Points: "50",
-    TimeLimit: "120 seconds",
-    NumberOfTestCases: "3",
-    Description: "This is a sample description of the challenge.",
-    InputFormat: "Expected input format details.",
-    Constraints: "List of constraints for the challenge.",
-    OutputFormat: "Expected output format details.",
+const ChallengeDetails = ({question,setQuestion}) => {
+  const [loading,setLoading] = useState(false)
+  const navigate = useNavigate();
+
+  const handleFieldChange = (event) => {
+    const { name, value } = event.target;
+    setQuestion((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
+
+
+  const handleUpdate = async (event) => {
+    event.preventDefault();
+    setLoading(true)
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}questions/${question.id}/`, {
+        method: 'PUT',
+        headers : {
+          'Content-Type': 'application/json',
+          "Authorization":`Bearer ${localStorage.getItem("accessToken")}`
+        },
+        body: JSON.stringify(question),
+      });
+      const data = await response.json()
+      if(!response.ok){
+        if (response.status === 401 || response.status === 403){
+          localStorage.removeItem("accessToken")
+          navigate("/")
+        }
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      setQuestion(data)
+      setLoading(false)
+      // Handle the response from the backend
+    } catch (error) {
+      // Handle errors
+      console.error(error);
+    } 
+  }
 
   return (
     <div className={styles.challengeDetailsContainer}>
       <h1 className={styles.title}>
         This is the basic information that describes your challenge.
       </h1>
+      {question && (
       <form className={styles.form}>
-        {Object.entries(challengeDetails).map(([key, value]) => (
+
+        {Object.entries(question).map(([key, value]) => (
+          key !== "id"  &&
+          key !== "contest" && 
           <div key={key} className={styles.formGroup}>
             <label className={styles.label}>
               {key.replace(/([A-Z])/g, " $1")}
@@ -30,16 +63,17 @@ const ChallengeDetails = () => {
               className={styles.value}
               style={{ "--content-width": `${value.length + 18}px` }}
               placeholder={value}
+              name={key} // Add the name attribute for form field
+              value={value}
+              onChange={handleFieldChange}
             ></input>
           </div>
         ))}
       </form>
+      )}
       <span className={styles.buttons}>
-        <button type="submit" className={styles.submitButton}>
-          Save Changes
-        </button>
-        <button type="submit" className={styles.updateButton}>
-          Update
+        <button type="submit" className={styles.submitButton} onClick={(event) => handleUpdate(event)}>
+          { loading ? "Updating....." : "Save Changes" }
         </button>
       </span>
     </div>
--- src/components/Dashboard/ParentComponent.jsx
+++ src/components/Dashboard/ParentComponent.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useState,useEffect} from "react";
 import TabsComponent from "./TabsComponent";
 import ChallengeDetails from "./ChallengeDetails";
 import TestCasesTab from "./TestCasesTab";
 import Sidebar from "./Sidebar";
 import styles from "./ParentComponent.module.css"; // Assume your CSS module for layout
+import {useNavigate} from 'react-router-dom';
 import SampleTestCases from "./SampleTestCases";
 import Constraints from "./Constraints";
 
+const CHALLENGE_ID = 28
 const ParentComponent = () => {
   const [activeTab, setActiveTab] = useState("Challenge");
+  const [testCases,setTestCases] = useState()
+  const [filesRequired,setFilesRequired] = useState(false)
+  const [avaiableTestCases,setAvailableTestCases] = useState()
+  const [question,setQuestion] = useState()
+  const [sample,setSample] = useState([])
+  const [constraints,setConstraints] = useState([])
 
   // Define the tab data
   const tabs = [
@@ -18,13 +26,60 @@ const ParentComponent = () => {
     { id: "Constraints", label: "Constraints" },
   ];
 
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}questions/${CHALLENGE_ID}`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+
+
+      const data = await response.json();
+        if (!response.ok) {
+          // 401 means unauthorized , 403 means unauthorized, so the user is either using an old token or is 
+          // either bypassing 
+          if (response.status === 401 || response.status === 403){
+            localStorage.removeItem("accessToken")
+            navigate("/")
+          }
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        setQuestion(data.question)
+        setTestCases(data.testcases.num_of_test_cases)
+        setAvailableTestCases(data.testcases.avaialabe_test_cases)
+        setFilesRequired(data.testcases.files_required)
+        setSample(data.samples)
+        setConstraints(data.consts)
+
+        // Process the data
+      } catch (error) {
+        // Handle errors
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   // Render content based on active tab
   const renderTabContent = (tab) => {
     switch (tab) {
       case "Challenge":
-        return <ChallengeDetails />;
+        return <ChallengeDetails question={question} setQuestion={setQuestion} />;
       case "TestCases":
-        return <TestCasesTab />;
+        return <TestCasesTab 
+        questionId={question.id} 
+        avaiableTestCases={avaiableTestCases} 
+        setAvailableTestCases={setAvailableTestCases} 
+        testCases={testCases} 
+        filesRequired={filesRequired} 
+        setFilesRequired={setFilesRequired} 
+        />;
       case "Samples":
         return <SampleTestCases />;
       case "Constraints":
