03cf5050e6459d1443eda9200d9c5298ac9dbed9
Elhamullah Hossaini <afghanbahram8@gmail.com>
2024-02-01 05:08:51 +0430

    added pagination to users list and challenge grid

--- src/components/ContestChallenges/ChallengesGrid.jsx
+++ src/components/ContestChallenges/ChallengesGrid.jsx
@@ -3,37 +3,42 @@ import ChallengeCard from "./ChallengeCard";
 import styles from "./ChallengesGrid.module.css"; // Path to your CSS module
 import NavBar from "../NavigationBar/NavBar"
 import {useNavigate} from 'react-router-dom';
+import Pagination from "../Pagination/Pagination";
 
 const ChallengesGrid = () => {
-  const [challengesData,setChallengesData] = useState([]);
-  const [contestName,setContestName] = useState("Contest")
+  const [challengesData, setChallengesData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [challengesPerPage] = useState(10); // Number of challenges per page
+  const [contestName, setContestName] = useState("Contest");
 
   const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}competition/`, {
-          method: "GET",
-          headers: {
-            "Authorization": `Bearer ${localStorage.getItem("accessToken")}`,
-          },
-        });
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}competition/`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+          }
+        );
 
-
-      const data = await response.json();
+        const data = await response.json();
         if (!response.ok) {
-          console.log(response.status)
-          // 401 means unauthorized , so the user is either using an old token or is 
-          // either bypassing 
-          if (response.status == 401){
-            localStorage.removeItem("accessToken")
-            navigate("/")
+          console.log(response.status);
+          // 401 means unauthorized , so the user is either using an old token or is
+          // either bypassing
+          if (response.status === 401) {
+            localStorage.removeItem("accessToken");
+            navigate("/");
           }
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-        console.log(data)
-        setChallengesData(data.data)
-        setContestName(data.name)
+        console.log(data);
+        setChallengesData(data.data);
+        setContestName(data.name);
         // Process the data
       } catch (error) {
         // Handle errors
@@ -43,15 +48,24 @@ const ChallengesGrid = () => {
 
     fetchData();
   }, []);
+
+  const indexOfLastChallenge = currentPage * challengesPerPage;
+  const indexOfFirstChallenge = indexOfLastChallenge - challengesPerPage;
+  const currentChallenges = challengesData.slice(
+    indexOfFirstChallenge,
+    indexOfLastChallenge
+  );
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <>
-    <NavBar />
-    <div className={styles.gridContainer}>
-      <h1 className={styles.title}>{contestName}</h1>
-      <div className={styles.cardsGrid}>
-        {
-          challengesData && (
-            challengesData.map((challenge, index) => (
+      <NavBar />
+      <div className={styles.gridContainer}>
+        <h1 className={styles.title}>{contestName}</h1>
+        <div className={styles.cardsGrid}>
+          {challengesData &&
+            currentChallenges.map((challenge, index) => (
               <ChallengeCard
                 key={challenge.id}
                 id={challenge.id}
@@ -59,15 +73,18 @@ const ChallengesGrid = () => {
                 points={challenge.point}
                 timeLimit={challenge.time_limit}
                 solved={challenge.solved}
-                lvl = {challenge.lvl}
+                lvl={challenge.lvl}
                 isOdd={index % 2 === 0}
                 description={challenge.description}
               />
-            ))
-            )
-          }
-          </div>
-    </div>
+            ))}
+        </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil(challengesData.length / challengesPerPage)}
+          onPageChange={paginate}
+        />
+      </div>
     </>
   );
 };
--- src/components/Dashboard/Users.jsx
+++ src/components/Dashboard/Users.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Users.module.css"; // CSS Module for Users
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrashAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import Pagination from "../Pagination/Pagination";
 
 const Users = () => {
   const navigate = useNavigate();
@@ -12,48 +13,82 @@ const Users = () => {
     { id: 2, name: "Beta" },
     { id: 3, name: "Omega" },
     { id: 4, name: "Gamma" },
+    { id: 5, name: "Alpha" },
+    { id: 6, name: "Beta" },
+    { id: 7, name: "Omegaa" },
+    { id: 8, name: "Gamma" },
+    { id: 9, name: "Alpha" },
+    { id: 10, name: "Beta" },
+    { id: 11, name: "Omega" },
+    { id: 12, name: "Gamma" },
+    { id: 13, name: "Alpha" },
+    { id: 14, name: "Beta" },
+    { id: 15, name: "Omega" },
+    { id: 16, name: "Gamma" },
+    { id: 17, name: "Alpha" },
+    { id: 18, name: "Beta" },
+    { id: 19, name: "Omega" },
+    { id: 20, name: "Gamma" },
   ];
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(5); // Number of users per page
+
+  // Get current users
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.usersContainer}>
-        <h1 className={styles.title}>Users</h1>
-        <button
-          className={styles.addButton}
-          onClick={() => navigate("/createUser")}
-        >
-          <FontAwesomeIcon icon={faPlus} />
-          Add New
-        </button>
-        <table className={styles.usersTable}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Team Names</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td>
-                  <FontAwesomeIcon
-                    icon={faEdit}
-                    className={styles.actionIcon}
-                  />
-                  <FontAwesomeIcon
-                    icon={faTrashAlt}
-                    className={styles.actionIcon}
-                  />
-                </td>
+    <>
+      <div className={styles.container}>
+        <div className={styles.usersContainer}>
+          <h1 className={styles.title}>Users</h1>
+          <button
+            className={styles.addButton}
+            onClick={() => navigate("/createUser")}
+          >
+            <FontAwesomeIcon icon={faPlus} />
+            Add New
+          </button>
+          <table className={styles.usersTable}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Team Names</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {currentUsers.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>
+                    <FontAwesomeIcon
+                      icon={faEdit}
+                      className={styles.actionIcon}
+                    />
+                    <FontAwesomeIcon
+                      icon={faTrashAlt}
+                      className={styles.actionIcon}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={Math.ceil(users.length / usersPerPage)}
+        onPageChange={paginate}
+      />
+    </>
   );
 };
 
