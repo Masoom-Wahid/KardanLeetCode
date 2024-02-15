b17adba3f67c514f4df9ad65f67f9bfb404c5895
Masoom Wahid <masoom3892@gmail.com>
2024-02-15 11:06:37 +0430

    Integration of Leaderboard

--- src/components/LeaderBoard/Organisms/LeaderboardPage.jsx
+++ src/components/LeaderBoard/Organisms/LeaderboardPage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Reports from "../../Dashboard/ManageContests/Reports/Reports";
 import "./LeaderboardTableOrganism.scss";
 
-const LeaderboardPage = () => {
+const LeaderboardPage = ({contestData}) => {
   return (
     <>
       <div variant="h4" className="leaderboard-title">
         Leaderboard
       </div>
-      <Reports />
+      <Reports contestData={contestData} />
     </>
   );
 };
